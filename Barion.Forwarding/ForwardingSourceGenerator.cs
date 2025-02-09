6e1d5c6a473ea1b9f614f590d9a0270ca7d3274c
Barion <ametrin.studios@gmail.com>
2025-02-09 20:30:26 +0100

    updates

--- Barion.Forwarding/ForwardingSourceGenerator.cs
+++ Barion.Forwarding/ForwardingSourceGenerator.cs
@@ -3,163 +3,153 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
 namespace Barion.Forwarding;
 
 [Generator]
-internal sealed class ForwardingSourceGenerator : ISourceGenerator {
-    private static string[] SymbolNameBlacklist = { "GetType" };
+internal sealed class ForwardingSourceGenerator : IIncrementalGenerator {
+    private static readonly string[] SymbolNameBlacklist = ["GetType"];
     public const string FORWARDING_ATTRIBUTE_NAME = "ForwardingAttribute";
     public const string FORWARD_ATTRIBUTE_NAME = "ForwardAttribute";
     public const string FORWARD_METHODS_ATTRIBUTE_NAME = "ForwardMethodsAttribute";
     public const string FORWARD_PROPERTIES_ATTRIBUTE_NAME = "ForwardPropertiesAttribute";
 
-    public void Initialize(GeneratorInitializationContext context) {
-//#if DEBUG
-//        if(!Debugger.IsAttached) {
-//            Debugger.Launch();
-//        }
-//#endif
+    public void Initialize(IncrementalGeneratorInitializationContext context) {
+        //#if DEBUG
+        //        if (!Debugger.IsAttached)
+        //        {
+        //            Debugger.Launch();
+        //        }
+        //#endif
 
-        context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        var nodes = context.SyntaxProvider.CreateSyntaxProvider(
+            static (node, _) => node is ClassDeclarationSyntax or StructDeclarationSyntax,
+            static (ctx, token) => (ctx.SemanticModel.GetDeclaredSymbol(ctx.Node, token) as INamedTypeSymbol)!
+        ).Where(c => c.HasAttribute(FORWARDING_ATTRIBUTE_NAME))
+        .Select((c, _) => (type: c, members: c.GetMembers(SymbolKind.Field, SymbolKind.Property).Where(m => m.GetAttributes().Any(a => a.AttributeClass!.Name is FORWARD_ATTRIBUTE_NAME or FORWARD_METHODS_ATTRIBUTE_NAME or FORWARD_PROPERTIES_ATTRIBUTE_NAME))))
+        .Where(p => p.members.Any());
+
+        context.RegisterSourceOutput(nodes, Execute);
     }
 
-    public void Execute(GeneratorExecutionContext context) {
-        if(context.SyntaxContextReceiver is not SyntaxReceiver receiver) return;
+    public void Execute(SourceProductionContext output, (INamedTypeSymbol type, IEnumerable<ISymbol> members) node) {
+        var (classSymbol, members) = node;
 
-        foreach(var typeDeclaration in receiver.CandidateTypes) {
-            var model = context.Compilation.GetSemanticModel(typeDeclaration.SyntaxTree);
-            if(model.GetDeclaredSymbol(typeDeclaration) is not INamedTypeSymbol classSymbol) continue;
-            if(!classSymbol.HasAttribute(FORWARDING_ATTRIBUTE_NAME)) continue;
+        var classSubMembers = classSymbol.BaseType!.GetAllMembers();
+        var builder = new ClassBuilder(classSymbol, classSymbol.ContainingNamespace);
 
-            var classSubMembers = classSymbol.BaseType!.GetAllMembers();
-            var builder = new ClassBuilder(classSymbol, classSymbol.ContainingNamespace);
-
-
-            foreach(var classMemberSymbol in classSymbol.GetMembers(SymbolKind.Field, SymbolKind.Property)) {
-                var type = classMemberSymbol is IFieldSymbol fieldSymbol ? fieldSymbol.Type : ((IPropertySymbol) classMemberSymbol).Type;
-                var allMembers = type.GetAllMembers().Where(CanSymbolBeForwarded);
-                var allMethods = allMembers.Where(symbol => symbol is IMethodSymbol).Cast<IMethodSymbol>();
-                var allProperties = allMembers.Where(symbol => symbol is IPropertySymbol).Cast<IPropertySymbol>();
+        foreach(var classMemberSymbol in members) {
+            var type = classMemberSymbol is IFieldSymbol fieldSymbol ? fieldSymbol.Type : ((IPropertySymbol) classMemberSymbol).Type;
+            var allMembers = type.GetAllMembers().Where(CanSymbolBeForwarded);
+            var allMethods = allMembers.OfType<IMethodSymbol>();
+            var allProperties = allMembers.OfType<IPropertySymbol>();
                 
-                var forwardPropertiesAttribute = classMemberSymbol.GetAttribute(FORWARD_PROPERTIES_ATTRIBUTE_NAME);
-                if(forwardPropertiesAttribute is not null) ForwardProperties(forwardPropertiesAttribute);
+            var forwardPropertiesAttribute = classMemberSymbol.GetAttribute(FORWARD_PROPERTIES_ATTRIBUTE_NAME);
+            if(forwardPropertiesAttribute is not null) ForwardProperties(forwardPropertiesAttribute);
 
-                var forwardMethodsAttribute = classMemberSymbol.GetAttribute(FORWARD_METHODS_ATTRIBUTE_NAME);
-                if(forwardMethodsAttribute is not null) ForwardMethods(forwardMethodsAttribute);
+            var forwardMethodsAttribute = classMemberSymbol.GetAttribute(FORWARD_METHODS_ATTRIBUTE_NAME);
+            if(forwardMethodsAttribute is not null) ForwardMethods(forwardMethodsAttribute);
                 
-                var forwardAttribute = classMemberSymbol.GetAttribute(FORWARD_ATTRIBUTE_NAME);
-                if(forwardAttribute is not null) Forward(forwardAttribute);                
+            var forwardAttribute = classMemberSymbol.GetAttribute(FORWARD_ATTRIBUTE_NAME);
+            if(forwardAttribute is not null) Forward(forwardAttribute);                
 
-                void Forward(AttributeData attribute) {
-                    var specifiedMemberNames = attribute.ConstructorArguments[0].Values.Select(value => (string)value.Value!).ToArray();
-                    ForwardPropertiesInternal(GetProperties(specifiedMemberNames));
-                    ForwardMethodsInternal(GetMethods(specifiedMemberNames));
-                }
+            void Forward(AttributeData attribute) {
+                var specifiedMemberNames = attribute.ConstructorArguments[0].Values.Select(value => (string)value.Value!).ToImmutableArray();
+                ForwardPropertiesInternal(GetProperties(specifiedMemberNames));
+                ForwardMethodsInternal(GetMethods(specifiedMemberNames));
+            }
                 
-                void ForwardMethods(AttributeData attribute) {
-                    var specifiedMemberNames = attribute.ConstructorArguments[0].Values.Select(value => (string)value.Value!).ToArray();
-                    ForwardMethodsInternal(GetMethods(specifiedMemberNames));
-                }
+            void ForwardMethods(AttributeData attribute) {
+                var specifiedMemberNames = attribute.ConstructorArguments[0].Values.Select(value => (string)value.Value!).ToImmutableArray();
+                ForwardMethodsInternal(GetMethods(specifiedMemberNames));
+            }
                 
-                void ForwardProperties(AttributeData attribute) {
-                    var includeSetter = (bool) attribute.ConstructorArguments[0].Value!;
-                    var specifiedMemberNames = attribute.ConstructorArguments[1].Values.Select(value => (string)value.Value!).ToArray();
+            void ForwardProperties(AttributeData attribute) {
+                var includeSetter = (bool) attribute.ConstructorArguments[0].Value!;
+                var specifiedMemberNames = attribute.ConstructorArguments[1].Values.Select(value => (string)value.Value!).ToImmutableArray();
 
-                    ForwardPropertiesInternal(GetProperties(specifiedMemberNames), includeSetter);
-                }
+                ForwardPropertiesInternal(GetProperties(specifiedMemberNames), includeSetter);
+            }
 
-                IEnumerable<IMethodSymbol> GetMethods(string[] whitelist) {
-                    if(whitelist.Length == 0) return allMethods.Where(member => !SymbolNameBlacklist.Contains(member.Name));
+            IEnumerable<IMethodSymbol> GetMethods(ImmutableArray<string> whitelist) {
+                if(whitelist.Length == 0) return allMethods.Where(member => !SymbolNameBlacklist.Contains(member.Name));
 
-                    return allMethods.Where(member => whitelist.Contains(member.Name));
-                }
+                return allMethods.Where(member => whitelist.Contains(member.Name));
+            }
                 
-                IEnumerable<IPropertySymbol> GetProperties(string[] whitelist) {
-                    if(whitelist.Length == 0) return allProperties.Where(member => !SymbolNameBlacklist.Contains(member.Name));
+            IEnumerable<IPropertySymbol> GetProperties(ImmutableArray<string> whitelist) {
+                if(whitelist.Length == 0) return allProperties.Where(member => !SymbolNameBlacklist.Contains(member.Name));
 
-                    return allProperties.Where(member => whitelist.Contains(member.Name));
-                }
+                return allProperties.Where(member => whitelist.Contains(member.Name));
+            }
 
-                void ForwardMethodsInternal(IEnumerable<IMethodSymbol> methodSymbols) {
-                    var forwarded = new List<IMethodSymbol>();
-                    foreach(var methodSymbol in methodSymbols) {
-                        var ignoredSymbol = classSubMembers.FirstOrDefault(symbol => symbol.Name == methodSymbol.Name);
-                        var shouldOverride = ignoredSymbol is not null;
+            void ForwardMethodsInternal(IEnumerable<IMethodSymbol> methodSymbols) {
+                var forwarded = new List<IMethodSymbol>();
+                foreach(var methodSymbol in methodSymbols) {
+                    var ignoredSymbol = classSubMembers.FirstOrDefault(symbol => symbol.Name == methodSymbol.Name);
+                    var shouldOverride = ignoredSymbol is not null;
 
-                        if(forwarded.Any(other => methodSymbol.Name == other.Name && methodSymbol.MatchParameters(other) && methodSymbol.ReturnType.Equals(other.ReturnType, SymbolEqualityComparer.Default))) continue;
+                    if(forwarded.Any(other => methodSymbol.Name == other.Name && methodSymbol.MatchParameters(other) && methodSymbol.ReturnType.Equals(other.ReturnType, SymbolEqualityComparer.Default))) continue;
 
-                        if(shouldOverride && classMemberSymbol.IsStatic) {
-                            shouldOverride = false;
-                        }
-
-                        if(shouldOverride && ignoredSymbol is not IMethodSymbol) {
-                            builder.Add($"//cannot override because base.{ignoredSymbol!.Name} is not a method");
-                            shouldOverride = false;
-                        }
-
-                        if(shouldOverride && !(ignoredSymbol!.IsVirtual || ignoredSymbol.IsOverride)) {
-                            builder.Add($"//cannot override because base.{ignoredSymbol.Name} is not virtual");
-                            shouldOverride = false;
-                        }
-
-                        if(shouldOverride && ignoredSymbol is IMethodSymbol ignoredMethod && !methodSymbol.MatchParameters(ignoredMethod)) {
-                            builder.Add($"//cannot override base.{ignoredSymbol!.Name} because of differend signatures");
-                            shouldOverride = false;
-                        }
-
-
-                        builder.ForwardMethod(methodSymbol, classMemberSymbol, shouldOverride);
-                        forwarded.Add(methodSymbol);
+                    if(shouldOverride && classMemberSymbol.IsStatic) {
+                        shouldOverride = false;
                     }
-                }
-                
-                void ForwardPropertiesInternal(IEnumerable<IPropertySymbol> propertySymbols, bool includeSetter = false) {
-                    foreach(var propertySymbol in propertySymbols) {
-                        var mightIgnoreMember = classSubMembers.FirstOrDefault(symbol => symbol.Name == propertySymbol.Name);
-                        var shouldOverride = mightIgnoreMember is not null;
 
-                        if(shouldOverride && classMemberSymbol.IsStatic) {
-                            shouldOverride = false;
-                        }
-
-                        if(shouldOverride && mightIgnoreMember is not IPropertySymbol) {
-                            builder.Add($"//cannot override because base.{mightIgnoreMember!.Name} is not a property");
-                            shouldOverride = false;
-                        }
-                        
-                        if(shouldOverride && !mightIgnoreMember!.IsVirtual) {
-                            builder.Add($"//cannot override because base.{mightIgnoreMember.Name} is not virtual");
-                            shouldOverride = false;
-                        }
-
-                        builder.ForwardProperty(propertySymbol, classMemberSymbol, includeSetter, shouldOverride);
+                    if(shouldOverride && ignoredSymbol is not IMethodSymbol) {
+                        builder.Add($"// cannot override because base.{ignoredSymbol!.Name} is not a method");
+                        shouldOverride = false;
                     }
+
+                    if(shouldOverride && !(ignoredSymbol!.IsVirtual || ignoredSymbol.IsOverride)) {
+                        builder.Add($"// cannot override because base.{ignoredSymbol.Name} is not virtual");
+                        shouldOverride = false;
+                    }
+
+                    if(shouldOverride && ignoredSymbol is IMethodSymbol ignoredMethod && !methodSymbol.MatchParameters(ignoredMethod)) {
+                        builder.Add($"// cannot override base.{ignoredSymbol!.Name} because of differend signatures");
+                        shouldOverride = false;
+                    }
+
+
+                    builder.ForwardMethod(methodSymbol, classMemberSymbol, shouldOverride);
+                    forwarded.Add(methodSymbol);
                 }
             }
-            builder.Close();
+                
+            void ForwardPropertiesInternal(IEnumerable<IPropertySymbol> propertySymbols, bool includeSetter = false) {
+                foreach(var propertySymbol in propertySymbols) {
+                    var mightIgnoreMember = classSubMembers.FirstOrDefault(symbol => symbol.Name == propertySymbol.Name);
+                    var shouldOverride = mightIgnoreMember is not null;
 
-            context.AddSource($"{builder.Name}.g.cs", builder.ToString());
+                    if(shouldOverride && classMemberSymbol.IsStatic) {
+                        shouldOverride = false;
+                    }
+
+                    if(shouldOverride && mightIgnoreMember is not IPropertySymbol) {
+                        builder.Add($"//cannot override because base.{mightIgnoreMember!.Name} is not a property");
+                        shouldOverride = false;
+                    }
+                        
+                    if(shouldOverride && !mightIgnoreMember!.IsVirtual) {
+                        builder.Add($"//cannot override because base.{mightIgnoreMember.Name} is not virtual");
+                        shouldOverride = false;
+                    }
+
+                    builder.ForwardProperty(propertySymbol, classMemberSymbol, includeSetter, shouldOverride);
+                }
+            }
         }
+        builder.Close();
 
-        return;
+        output.AddSource($"{builder.Name}.g.cs", builder.ToString());
 
         static bool CanSymbolBeForwarded(ISymbol symbol) {
             return !symbol.IsStatic &&
-                symbol.DeclaredAccessibility is Accessibility.Public or Accessibility.Internal &&
+                symbol.DeclaredAccessibility is Accessibility.Public && // TODO: allow internal in the same namespace
                 !(symbol.Name.StartsWith("set_") || symbol.Name.StartsWith("get_")) &&
                 symbol.Name != ".ctor";
-        }
-    }
-
-    private class SyntaxReceiver : ISyntaxContextReceiver {
-        public List<TypeDeclarationSyntax> CandidateTypes { get; } = new();
-
-        public void OnVisitSyntaxNode(GeneratorSyntaxContext context) {
-            if(context.Node is ClassDeclarationSyntax or StructDeclarationSyntax) {
-                CandidateTypes.Add(context.Node as TypeDeclarationSyntax);
-            }
         }
     }
 }
